ff847c4d61875ef7e2b94338498c71ee4a112238
kadarakos <kadar.akos@gmail.com>
2020-05-18 23:34:41 -0400

    Initial commit.

---
+++ hmm.py
@@ -0,0 +1,155 @@
+"""
+Hints from https://web.stanford.edu/~jurafsky/slp3/8.pdf
+"""
+
+import math
+import time
+import numpy
+from utils import read_train_data
+from utils import ngrams
+import tqdm
+
+class HMM:
+    def __init__(self, unk_freq=1):
+
+        self.unk_freq = unk_freq
+        self.sentences = []
+        self.targets = []
+        self.tags = []
+        self.word_counts = {}
+        self.pos_unigram_counts = {}
+        self.emission_counts = {}
+        self.transition_counts = {}
+        self.emission_probs = {}
+        self.transition_probs = {}
+        self.pos_unigram_probs = {}
+        self.word_counts = {}
+
+    def get_word_counts(self, sentences):
+        for s in sentences:
+            for w in s:
+                self.word_counts[w] = self.word_counts.get(w, 0.0) + 1
+
+    def replace_UNK(self, sentences, freq=1):
+        """
+        Replace the words that appear less than freq times in
+        the emission counts.
+        """
+        #FIXME naive way.
+        self.low_freq_words = set([])
+        for w, c in self.word_counts.items():
+            if c <= freq:
+                self.low_freq_words.add(w)
+        for i in range(len(sentences)):
+            for j in range(len(sentences[i])):
+                if sentences[i][j] in self.low_freq_words:
+                    sentences[i][j] = "<UNK>"
+
+    def compute_counts(self, sentences, targets):
+        """
+        Load text and estimate parameters for bigram HMM
+
+        :param path: path to data file
+        """
+        for i in range(len(sentences)):
+            # When at the end of a sentence
+            # update counts
+            words = sentences[i]
+            tags = targets[i]
+            for i in range(len(tags)):
+                self.emission_counts[(words[i], tags[i])] = self.emission_counts.get((words[i], tags[i]), 0.0) + 1
+            # Adding start to have estimate for P(t_i|START) usually denote pi
+            pos_bigrams = ngrams(['<START>'] + tags, r=2, pad=False)
+            for gram in pos_bigrams.values():
+                # Update unigram counts
+                if len(gram) == 1:
+                    self.pos_unigram_counts[gram[0]] = self.pos_unigram_counts.get(gram[0], 0.0) + 1
+                else:
+                    self.transition_counts[gram] = self.transition_counts.get(gram, 0.0) + 1
+
+    def estimate_params(self):
+        """
+        Estimate the probabilities for HMM:
+        Emission probabilities and transition probabilities as usual.
+        """
+        for (w, t), c in self.emission_counts.items():
+            # P(w_i|t_i) = Count(w_i, t_i) / Count(t_i)
+            self.emission_probs[(w, t)] = c / self.pos_unigram_counts[t]
+            for t1, t2 in self.transition_counts:
+            # P(t_i|t_i-1) = Counts(t_i, t_i-1)/Count(t_i-1)
+                self.transition_probs[(t1, t2)] = self.transition_counts[(t1, t2)] / self.pos_unigram_counts[t1]
+            Z = sum(self.pos_unigram_counts.values())
+            for t in self.pos_unigram_counts:
+                self.pos_unigram_probs[t] = self.pos_unigram_counts[t] / Z
+        return self.emission_probs, self.transition_probs
+
+    def viterbi(self, S):
+        """
+        Viterbi decoding for a single example.
+        Given a tokenized sentence S, return tag sequence
+        http://www.cs.virginia.edu/~kc2wc/teaching/NLP16/slides/11-Viterbi.pdf
+        slide 38
+        pseudo code: https://gist.github.com/aaronj1335/9650261
+        """
+        # Viterbi table is sentence length by number of pos tags
+        T = numpy.zeros((len(S), len(self.tags))) - numpy.inf
+        backpointers = numpy.zeros((len(S), len(self.tags)))
+        # Initialize with log(P(w_1|t_i)) + log(P(t_i|START))
+        w1 = S[0] if S[0] in self.word_counts else "<UNK>"
+        for i, tag in enumerate(self.tags):
+            pw1_ti = self.emission_probs.get((w1, tag), 1e-8)
+            pti_tim1 = self.transition_probs.get(('<START>', tag), 1e-8)
+            T[0, i] = math.log(pw1_ti) + math.log(pti_tim1)
+        # For each time-step in the sentence.
+        for i in range(1, len(S)):
+            # For each current tag.
+            for t in range(len(self.tags)):
+                w = S[i] if S[i] in self.word_counts else "<UNK>"
+                # P(w_i|t_i)
+                pw_ti = self.emission_probs.get((w, self.tags[t]), 1e-8)
+                # For each previous tag.
+                for tm1 in range(len(self.tags)):
+                    # P(t_i|t_m1)
+                    pti_tim1 = self.transition_probs.get((self.tags[tm1], self.tags[t]), 1e-8)
+                    transition = T[i-1, tm1] + math.log(pti_tim1)
+                    if transition > T[i, t]:
+                        T[i, t] = transition
+                        backpointers[i, t] = tm1
+                T[i, t] += math.log(pw_ti)
+        # Work back in time through the backpointers to get the best sequence
+        predicted_tags = [None for x in range(len(S))]
+        predicted_tags[0] = self.tags[numpy.argmax(T[0, :])]
+        predicted_tags[-1] = self.tags[numpy.argmax(T[-1, :])]
+        for i in range(len(S)-2, 0, -1):
+            ind = numpy.argmax(T[i, :])
+            tag = self.tags[int(backpointers[i+1, ind])]
+            predicted_tags[i] = tag
+        return predicted_tags
+
+    def train(self, sentences, targets):
+        # Get word-counts in the training set
+        self.get_word_counts(sentences)
+        # Use counts to replace infrequent words with UNK
+        self.replace_UNK(sentences, self.unk_freq)
+        # Get counts to compute transition and emission probs later
+        self.compute_counts(sentences, targets)
+        # Compute P(w|t) and P(ti|ti-1)
+        self.estimate_params()
+        self.tags = list(self.pos_unigram_counts.keys())
+        self.tags.remove("<START>")
+
+train_sentences, train_targets = read_train_data('train.txt')
+test_sentences, test_targets = read_train_data('test.txt')
+hmm = HMM(unk_freq=1)
+hmm.train(train_sentences, train_targets)
+print(hmm.viterbi("Laura does not have an example".split()))
+num_tokens = 0
+correct = 0
+
+# Compute accuracy on the test set
+#for i, s in tqdm.tqdm(enumerate(test_sentences)):
+#    tags = hmm.viterbi(s)
+#    for j, t in enumerate(tags):
+#        num_tokens += 1.0
+#        correct += int(test_targets[i][j] == t)
+#print(correct/num_tokens)
---
+++ utils.py
@@ -0,0 +1,50 @@
+def ngrams(s, r, pad=False):
+    """
+    Takes a list as input and returns all n-grams up to a certain range.
+
+    :param s: input list
+    :param r: range
+    :param pad: whether to return ngrams containing a "PAD" token
+    :return: dicttionary {id: ngram}
+    """
+    # add padding
+    S = s + ["<PAD>"] * (r - 1)
+    l = len(S)
+    ng = {}
+    curr_id = 0
+    seen = set()
+    for i in range(l-1):
+        for j in range(i, i+r):
+            gram = tuple(S[i:j+1])
+            elements = set(gram)
+            # Don't store ngrams with PAD tokens
+            if "<PAD>" in elements and not pad:
+                continue
+            # Don't store ngrams with only the pad token
+            elif len(elements) == 1 and "<PAD>" in elements:
+                continue
+            elif gram not in seen:
+                seen.add(gram)
+                ng[curr_id] = gram
+                curr_id += 1
+    return ng
+
+def read_train_data(path):
+    sentences = []
+    targets = []
+    with open(path, 'r') as f:
+        words = []
+        tags = []
+        for line in f:
+            if line == "\n":
+                if len(tags) != len(words):
+                    raise
+                sentences.append(words)
+                targets.append(tags)
+                words = []
+                tags = []
+            else:
+                word, pos, chunk = line.split()
+                words.append(word)
+                tags.append(pos)
+    return sentences, targets
